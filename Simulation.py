b2f3540d35728607eb38287c6c57db5f07ca413a
jzheng07 <91685349+jzheng07@users.noreply.github.com>
2021-11-11 00:57:02 -0500

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -70,67 +70,67 @@ def c3():
 	return random.normalvariate(c3avg, c3std)
 #eGreedy(e) starts here, input: e -> is percentage, e%
 def eGreedy(e):
-	#First Three day, go to each cafeteria and get the happiness value
-	result = 0 #Result
-	happiness = [] #Empty list to put happy score in
-	happiness.append(c1()) #append happiness into the list
-	happiness.append(c2())
-	happiness.append(c3())
-	result += sum(happiness) #add three days' happyiness point
-	#initial int visit times. Initial from one because you already viisted each cafeteria onece
-	c1n = 1
-	c2n = 1
-	c3n = 1
-	#The rest 297 days
-	for i in range (297):#Loop starts here
-		#Generate a random number 0 to 100
-		r = 100 * random.random()
-		#Generate a random number 0 to 2(0,1,2,3)
-		x = random.randrange(3)
-		#If the random number r is smaller than e, pick a random cafeteria
-		if (r < e):
-			#according the random number x, if x is 0 go to cafeteria one
-			if (x == 0):
-				c1n += 1 #add visit times
-				c1h = c1() #get happiness core using helper function
-				happiness[0] = (happiness[0] + c1h) / c1n #modify the happiness score for this cafeteria so far
-				result += c1h #add the happiness score to the total result
-			elif (x == 1):
-			# if random num x is 1, go to the cafeteria two
-				c2n += 1 
-				c2h = c2()
-				happiness[1] = (happiness[1] + c2h) / c2n
-				result += c2h
-			else:
-			#the last situation is random num x is three, go to the cafeteria three
-				c3n += 1
-				c3h = c3()
-				happiness[2] = (happiness[2] + c3h) / c3n
-				result += c3h
-		else:
-		#Otherwise, go to the cafeteria earn the best happiness so far
-			y = happiness.index(max(happiness))#from the happiness list, get the index has the highest number is the best happiness cafeteria so far
-			#If first number is the highest, go to the cafeteria one
-			if (y == 0):
-				c1n += 1 #add visit time
-				c1h = c1() #get happiness score by using helper function
-				happiness[0] = (happiness[0] + c1h) / c1n #update the average happiness score 
-				result += c1h # add to result
-			#if the second number is the highest, go to the cafeteria two
-			elif (y == 1):
-				c2n += 1
-				c2h = c2()
-				happiness[1] = (happiness[1] + c2h) / c2n
-				result += c2h
-			#The last situation, the last num is the highest in the list, go to the cafeteria three
-			else:
-				c3n += 1
-				c3h = c3()
-				happiness[2] = (happiness[2] + c3h) / c3n
-				result += c3h
-		
-	#Return the happiness score
-	return result
+    # First Three day, go to each cafeteria and get the happiness value
+    result = 0  # Result
+    happiness = []  # Empty list to put happy score in
+    happiness.append(c1())  # append happiness into the list
+    happiness.append(c2())
+    happiness.append(c3())
+    result += sum(happiness)  # add three days' happyiness point
+    # initial int visit times. Initial from one because you already viisted each cafeteria onece
+    c1n = 1
+    c2n = 1
+    c3n = 1
+    # The rest 297 days
+    for i in range(297):  # Loop starts here
+        # Generate a random number 0 to 100
+        r = 100 * random.random()
+        # Generate a random number 0 to 2(0,1,2,3)
+        x = random.randrange(3)
+        # If the random number r is smaller than e, pick a random cafeteria
+        if (r < e):
+            # according the random number x, if x is 0 go to cafeteria one
+            if (x == 0):
+                c1n += 1  # add visit times
+                c1h = c1()  # get happiness core using helper function
+                happiness[0] = (happiness[0] + c1h)  # modify the happiness score for this cafeteria so far
+                result += c1h  # add the happiness score to the total result
+            elif (x == 1):
+                # if random num x is 1, go to the cafeteria two
+                c2n += 1
+                c2h = c2()
+                happiness[1] = (happiness[1] + c2h)
+                result += c2h
+            else:
+                # the last situation is random num x is three, go to the cafeteria three
+                c3n += 1
+                c3h = c3()
+                happiness[2] = (happiness[2] + c3h)
+                result += c3h
+        else:
+            # Otherwise, go to the cafeteria earn the best happiness so far
+            y = (max(happiness[0]/c1n,happiness[1]/c2n,happiness[2]/c3n))  # from the happiness list, get the index has the highest number is the best happiness cafeteria so far
+            # If first number is the highest, go to the cafeteria one
+            if (y == happiness[0]/c1n):
+                c1n += 1  # add visit time
+                c1h = c1()  # get happiness score by using helper function
+                happiness[0] = (happiness[0] + c1h)  # update the happiness score
+                result += c1h  # add to result
+            # if the second number is the highest, go to the cafeteria two
+            elif (y == happiness[1]/c2n):
+                c2n += 1
+                c2h = c2()
+                happiness[1] = (happiness[1] + c2h)
+                result += c2h
+            # The last situation, the last num is the highest in the list, go to the cafeteria three
+            else:
+                c3n += 1
+                c3h = c3()
+                happiness[2] = (happiness[2] + c3h)
+                result += c3h
+
+    # Return the happiness score
+    return result
 
 #Junjie Zheng's part
 #simulation(t,e) int t -> is the times of trail; int e -> is the given percentage(0-100) use for eGreedy(e)
